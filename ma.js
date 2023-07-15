1dca012f0fd442c42f77db627e2dfdacf63f52b5
angel <mather@.com>
2023-07-15 00:44:19 -0300

    pueva precentacion

--- ma.js
+++ ma.js
@@ -13,29 +13,26 @@ header.addEventListener('click', () => {
     vadition = !vadition;
     if(vadition){
         bienvenida.style.visibility = 'visible';
-        navegador.style.marginTop = '3rem'
+        navegador.style.marginTop = '3.2rem'
         navegador.style.height = '2.5rem'
     }else{
         bienvenida.style.visibility = 'hidden';
-        navegador.style.height =  '2rem';
+        navegador.style.height =  '1.5rem';
         navegador.style.marginTop = '10px';
     }
+});
     
-});
-
-const contacto = document.getElementById('contacto');
-
-const icons = document.getElementById('icons-socials');
-
-let validatio = false;
-
-contacto.addEventListener('click', () =>{
-    validatio = !validatio;
-    if(validatio){
-        icons.style.display = 'flex';
-    }else{
-        icons.style.display = 'none'
+    var prevScrollpos = window.pageYOffset;
+    
+    window.addEventListener('scroll', function() {
+    var currentScrollPos = window.pageYOffset;
+    
+    if (prevScrollpos > currentScrollPos) {
+        header.style.top = "0";
+        navegador.style.display = "block";
+    } else {
+        header.style.top = "-180px";
+        navegador.style.display = "none"
     }
-});
-
-
+    prevScrollpos = currentScrollPos;
+    });
